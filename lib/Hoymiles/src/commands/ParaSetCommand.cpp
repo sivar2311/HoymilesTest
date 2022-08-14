f555f66cb7aa82936122e8006a8109c5adf9d686
sivar2311 <sivar2311@gmail.com>
2022-08-14 09:30:20 +0200

    Moved Hoymiles library to external repository

--- lib/Hoymiles/src/commands/ParaSetCommand.cpp
+++
@@ -1,7 +0,0 @@
-#include "ParaSetCommand.h"
-
-ParaSetCommand::ParaSetCommand(uint64_t target_address, uint64_t router_address)
-    : CommandAbstract(target_address, router_address)
-{
-    _payload[0] = 0x52;
-}
